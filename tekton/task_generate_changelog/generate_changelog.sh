11437a4456f4ad82b1b55dcef5d25d3c0d41126b
Stephan Michard <github@michard.io>
2024-01-18 10:11:19 +0100

    fix: :art: changelog file renamed

--- tekton/task_generate_changelog/generate_changelog.sh
+++ tekton/task_generate_changelog/generate_changelog.sh
@@ -8,7 +8,7 @@ trap 'echo "An error occurred at line $LINENO. Exiting."' ERR
 
 # Path to the Git repository (current directory)
 REPO_DIR="."
-CHANGELOG_FILE="$REPO_DIR/CHANGELOG_new.md"
+CHANGELOG_FILE="$REPO_DIR/CHANGELOG.md"
 GITHUB_REPO_URL="https://github.com/smichard/cities_demo" # Replace with your repository URL
 
 echo "Starting changelog generation script..."
